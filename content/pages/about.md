b424826d5605dc75fc212863c3f179615129d40b
Adam Saleh <asaleh@redhat.com>
2016-12-09 07:23:07 +0000

    Attempting first publish

---
+++ content/pages/about.md
@@ -0,0 +1,10 @@
+Title: About
+Date: 2010-12-03 10:20
+Modified: 2010-12-05 19:30
+Category: Python
+Tags: pelican, publishing
+Slug: my-super-post
+Authors: Adam Saleh
+Summary: About this site
+
+This site contains mostly notes of Adam Saleh a QE engineer in Red Hat
---
+++ pelicanconf.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Adam Saleh'
+SITENAME = 'asaleh/notebook'
+SITEURL = 'https://jupyter-blog-adamsaleh.c9users.io'
+SITETITLE = AUTHOR
+SITESUBTITLE = 'QE Engineer'
+SITEDESCRIPTION = 'Programming Languages Enthusiast'
+
+THEME = '/usr/local/lib/python3.4/dist-packages/pelican/themes/Flex'
+
+PATH = 'content'
+SITELOGO = SITEURL + '/images/profile.png'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+PLUGIN_PATHS = ['/home/ubuntu/workspace/adamsaleh/plugins']
+MARKUP = ('md', 'ipynb')
+
+PLUGINS = ['ipynb.markup','filetime_from_git']
+
+# Blogroll
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/adamsaleh'),)
+
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
+
+MENUITEMS = (('Archives', '/archives.html'),
+             ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
+
+DEFAULT_PAGINATION = False
+
+IGNORE_FILES = ['.ipynb_checkpoints']
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
